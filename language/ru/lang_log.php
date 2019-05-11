bdfbd19feda6d0fd612a5a0b59c0d1e33b9920da
AlexSheer <alex_sheer@inbox.lv>
2019-05-11 22:17:31 +0300

    Create lang_log.php

---
+++ language/ru/lang_log.php
@@ -0,0 +1,7 @@
+<?php
+$lang['L_LOG_DELETE']="Удалить лог";
+$lang['L_LOGFILEFORMAT']="Формат лог-файла";
+$lang['L_LOGFILENOTWRITABLE']="Невозможно записать файл журнала!";
+$lang['L_NOREVERSE']="Старая запись первой";
+$lang['L_REVERSE']="Последняя запись первой";
+?>
